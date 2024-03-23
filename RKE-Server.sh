9e0e179b7bc8eec6b9eb20f9d92530265c4be74b
Emanuel Ribeiro <emanuel.ribeiro@zello.tec.br>
2024-03-22 22:48:35 -0300

    fix script, changing k8s version

--- RKE-Server.sh
+++ RKE-Server.sh
@@ -42,9 +42,9 @@ systemctl disable --now ufw
 
 print_color "green" "Atualizando os pacotes e instalando dependências... "
 apt update
-apt install -y nfs-common cryptsetup open-iscsi tar
-systemctl enable --now open-iscsi
+apt install nfs-common -y  
 apt upgrade -y
+apt autoremove -y
 
 # Verificar se o UFW está rodando
 check_service_status ufw
@@ -55,11 +55,11 @@ print_color "green" "---------------- Configurando RKE2 Server -----------------
 
 # Instalar o RKE2
 print_color "green" "Instalando RKE2-Server com Kubernetes 1.24..."
-curl -sfL https://get.rke2.io | INSTALL_RKE2_CHANNEL=v1.24 INSTALL_RKE2_TYPE=server sh -
+curl -sfL https://get.rke2.io | INSTALL_RKE2_CHANNEL=v1.26 INSTALL_RKE2_TYPE=server sh -
 
 print_color "green" "Iniciando RKE-Server..."
-systemctl enable --now rke2-server
-systemctl start rke2-server
+systemctl enable --now rke2-server.service
+systemctl status rke2-server
 
 # Verificar se o RKE está rodando
 check_service_status rke2-server
@@ -70,16 +70,15 @@ print_color "green" "---------------- Configurando Kubectl ------------------"
 
 # Configurar o kubectl
 print_color "green" "Criando link simbólico para o kubectl..."
-ln -s /var/lib/rancher/rke2/bin/kubectl /usr/local/bin/kubectl
-export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
-echo 'export KUBECONFIG=/etc/rancher/rke2/rke2.yaml' >> ~/.bashrc
-cp /etc/rancher/rke2/rke2.yaml ~/.kube/config
+ln -s $(find /var/lib/rancher/rke2/data/ -name kubectl) /usr/local/bin/kubectl
+export KUBECONFIG=/etc/rancher/rke2/rke2.yaml 
 
 print_color "green" "Verificando se o nó subiu..."
 kubectl get node
 
 print_color "green" "Armazenando token da master..."
-cat /var/lib/rancher/rke2/server/token > /opt/node-token.txt
+cat /var/lib/rancher/rke2/server/node-token
+cat /var/lib/rancher/rke2/server/node-token > /opt/node-token.txt
 
 print_color "green" "---------------- Configuração do Kubectl - Finalizada ------------------"
 print_color "green" "---------------- TOKEN DISPONÍVEL EM /opt/node-token.txt ------------------"
